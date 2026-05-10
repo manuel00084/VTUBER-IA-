567ed9ec43b82f49c9e1f86988b3d3b51457e874
manuel00084 <manuel00084@gmail.com>
2026-05-09 20:08:30 -0600

    Add files via upload

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,9 @@
+from .ptt import PTTManager
+from .translator import TranslatorManager, MODOS, MODO_DEFAULT
+from .game_watcher import GameWatcher
 
+try:
+    from PIL import ImageGrab
+    PIL_OK = True
+except ImportError:
+    PIL_OK = False
